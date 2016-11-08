327648b592a6696062a102ca5f2f4776002c81f9
Clifford Snow <clifford@snowandsnow.us>
2016-11-07 19:28:01 -0800

    revised add_unit to capture other types

--- src/add_unit.sql
+++ src/add_unit.sql
@@ -1,1 +1,1 @@
-﻿update bellingham_addr set unit = regexp_replace(main_addre, '[0-9A-Z ]+ STE ','') where main_addre like '% STE %'
+﻿UPDATE bellingham_addr SET unit = REGEXP_REPLACE(main_addre, '[0-9A-Z ]+ (STE|APT|BLDG) ' ,'') WHERE main_addre SIMILAR TO '% (STE|APT|BLDG) %'
