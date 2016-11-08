5920e089fc9496530dc4a076c7518d3aaca3e81a
Clifford Snow <clifford@snowandsnow.us>
2016-11-08 11:11:51 -0800

    more fixes

--- src/bellingham_osm.sh
+++ src/bellingham_osm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ ! -d /home/clifford/OSM/Whatcom/Bellingham/osm/ ]
+cd ~/OSM/Whatcom/Bellingham
+
+if [ ! -d osm ]
 then
-	mkdir /home/clifford/OSM/Whatcom/Bellingham/osm/ 
+	mkdir osm 
 fi
 	
 
@@ -12,7 +14,7 @@ do
 
     if [ -e /tmp/${id}a.shp ]
     then
-      echo "Removing old files"
+#      echo "Removing old files"
       rm /tmp/${id}a.shp
       rm /tmp/${id}a.shx
       rm /tmp/${id}a.prj
@@ -25,16 +27,18 @@ do
       rm /tmp/${id}b.osm
     fi
 
-    if [ -e ~/OSM/Whatcom/Bellingham/osm/${id}.osm.gz ]
+    if [ -e osm/${id}.osm.gz ]
     then
-      rm ~/OSM/Whatcom/Bellingham/osm/${id}.osm.gz
+      rm osm/${id}.osm.gz
     fi
 
     pgsql2shp -f /tmp/${id}b -h localhost -u postgres mygis "SELECT b.* FROM bellingham_ba b, whatcom_votdst v WHERE ST_CONTAINS(v.geom, st_centroid(b.geom)) AND vtdst10 = '${id}'"
     pgsql2shp -f /tmp/${id}a -h localhost -u postgres mygis "SELECT a.* FROM bellingham_ao a, whatcom_votdst v WHERE ST_CONTAINS(v.geom, a.geom) AND vtdst10 = '${id}'"
-    ~/Development/ogr2osm/ogr2osm.py -f -t ~/OSM/Whatcom/Bellingham/bellingham_bldg.py /tmp/${id}b.shp -o /tmp/${id}b.osm
-    ~/Development/ogr2osm/ogr2osm.py -f -t ~/OSM/Whatcom/Bellingham/bellingham_addr.py /tmp/${id}a.shp -o /tmp/${id}a.osm
-    python ~/OSM/Whatcom/Bellingham/merge_osm2.py /tmp/${id}b.osm /tmp/${id}a.osm ${id}.osm
-    mv ${id}.osm ~/OSM/Whatcom/Bellingham/osm
-    gzip ~/OSM/Whatcom/Bellingham/osm/${id}.osm
+    ~/Development/ogr2osm/ogr2osm.py -f -t bellingham_bldg.py /tmp/${id}b.shp -o /tmp/${id}b.osm
+    ~/Development/ogr2osm/ogr2osm.py -f -t bellingham_addr.py /tmp/${id}a.shp -o /tmp/${id}a.osm
+
+# merge_osm2 merges two .osm files, one containing polygons the other points. 
+    python merge_osm2.py /tmp/${id}b.osm /tmp/${id}a.osm ${id}.osm
+    mv ${id}.osm osm
+    gzip osm/${id}.osm
 done < votdst.lst
