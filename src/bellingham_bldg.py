1e075303ec555701203c790e6af6bec44e49d0ca
Alan McConchie <alan.mcconchie@gmail.com>
2017-01-17 08:50:48 -0800

    fix more attributes and names

--- src/bellingham_bldg.py
+++ src/bellingham_bldg.py
@@ -25,13 +25,39 @@ def filterTags(attrs):
         if attrs['BLDGTYPE'] == 'CABIN':
             tags['building'] = 'yes'
         if attrs['BLDGTYPE'] == 'DUPLX':
-            tags['building'] = 'house'
+            tags['building'] = 'residential'
         if attrs['BLDGTYPE'] == 'HOUSE':
             tags['building'] = 'house'
         if attrs['BLDGTYPE'] == 'MOBIL':
-            tags['building'] = 'mobile_home'
+            tags['building'] = 'mobile_home'  # or static_caravan?
+    if 'TYPE' in attrs and attrs['TYPE'] != '':
+        if attrs['TYPE'] == 'TRAILER':
+            tags['building'] = 'static_caravan'
+        if attrs['TYPE'] == 'PUBLIC':
+            tags['building'] = 'public'
+        if attrs['TYPE'] == 'RESERVOIR':
+            tags['building'] = 'yes'
+            tags['man_made'] = 'storage_tank'
     if 'FLOORS' in attrs and attrs['FLOORS'] != '':
         tags['building:levels'] = attrs['FLOORS']
+    if 'NAME' in attrs and attrs['NAME'] != '':
+        name = attrs['NAME'].title()  # Probably shouldn't titlecase everything
+        if name[-3:] == ' Es': name = name[:-3] + ' Elementary School'
+        if name[-3:] == ' Ms': name = name[:-3] + ' Middle School'
+        if name[-3:] == ' Hs': name = name[:-3] + ' High School'
+        if name[:3] == 'Bfd':  name = 'BFD' + name[3:]
+        if name[:3] == 'Bsd':  name = 'BSD' + name[3:]
+        if name[:2] == 'Cp':   name = 'CP'  + name[2:]
+        if name[:2] == 'Fd':   name = 'FD'  + name[2:]
+        if name[:3] == 'Us ':  name = 'US ' + name[3:]
+        if name[:3] == 'Wsu':  name = 'WSU' + name[3:]
+        if name[:3] == 'Wta':  name = 'WTA' + name[3:]
+        if name[:3] == 'Wwu':  name = 'WWU' + name[3:]
+        if name[:4] == 'Ymca': name = 'YMCA' + name[4:]
+        # if name ends with a 2-letter WWU bldg appreviation, fix that w/ a regex?
+        tags['name'] = name
+    if 'YRBUILT' in attrs and attrs['YRBUILT'] > 1800:
+        tags['start_date'] = attrs['YRBUILT']
 
 
     return tags
